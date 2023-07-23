7f565037856aee7eebea26dd5af83489e9ccdc77
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-24 02:41:55 +0300

    🚀feat(users) add createUserDto and user.entity ⚙️chore(users) add store method to users.controller 🛠️refactor(users) implement findAll, find, and create methods in users.service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
+    constructor(private usersService: UsersService) { }
+
     @Get()
-    index(): any {
-        return [{ id: 1, name: 'John Doe'}];
+    index(): User[] {
+        return this.usersService.findAll();
     }
     @Get(':id')
-    show(@Param('id') id: number): any {
-        return { id, name: 'John Doe'};
+    show(@Param('id') id: string): User {
+        return this.usersService.find(Number(id));
+    }
+
+    @Post()
+    store(@Body() body: CreateUserDto): User {
+        return this.usersService.create(body)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users = [
+        {
+            id: 1,
+            name: 'John Doe',
+        },
+        {
+            id: 2,
+            name: 'Alice Caeiro',
+        },
+        {
+            id: 3,
+            name: 'Who Knows',
+        },
+        {
+            id: 4,
+            name: 'Another One',
+        }
+    ]
+
+    findAll(): any[] {
+        return this.users;
+    }
+
+    find(id: number): any {
+        return this.users.find(user => user.id === id);
+    }
+
+    create(createUserDto: CreateUserDto): any {
+        const user = { id: this.users.length + 1, ...createUserDto}
+        this.users.push(user);
+        return user;
+    }
+
+    update(id: number, data: any): any {
+        const user = this.find(id);
+        const index = this.users.indexOf(user);
+        this.users[index] = data;
+        return data;
+    }
+
+    delete(id: number): any[] {
+        const user = this.find(id);
+        const index = this.users.indexOf(user);
+        this.users.splice(index, 1);
+        return this.users;
+    }
+}
