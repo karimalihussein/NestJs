d18c0e339b516a5fd209c2833cc96a6e83c8d4f9
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-24 03:33:02 +0300

    🚀feat(dependencies) add class-transformer and class-validator ⚙️chore(dependencies) upgrade validator to 13.9.0 🛠️refactor(main) use global validation pipe 🛠️refactor(create-user.dto) add class-validator decorators ⚙️chore(users.controller) add ParseIntPipe to id parameter 🛠️refactor(users.controller) add BadRequest response

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsAlphanumeric, MaxLength } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty()
+    @IsAlphanumeric()
+    @MaxLength(20)
     readonly name: string;
     @ApiProperty({ required: false})
     readonly age?: number;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
-import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('users')
 @Controller('users')
@@ -18,12 +18,13 @@ export class UsersController {
     @Get(':id')
     @ApiOkResponse({ type: User })
     @ApiNotFoundResponse({ description: 'User not found' })
-    show(@Param('id') id: string): User {
+    show(@Param('id', ParseIntPipe) id: string): User {
         return this.usersService.find(Number(id));
     }
 
     @Post()
     @ApiCreatedResponse({ type: User })
+    @ApiBadRequestResponse({ description: 'Bad request' })
     store(@Body() body: CreateUserDto): User {
         return this.usersService.create(body)
     }
