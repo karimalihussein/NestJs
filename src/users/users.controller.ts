409cf21aa916d747ce5270cb02902ec505927bfc
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-24 03:07:08 +0300

    🛠️refactor(users) add API responses to user endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('users')
 @Controller('users')
@@ -10,10 +10,12 @@ export class UsersController {
     constructor(private usersService: UsersService) { }
 
     @Get()
+    @ApiOkResponse({ type: User, isArray: true })
     index(): User[] {
         return this.usersService.findAll();
     }
     @Get(':id')
+    @ApiOkResponse({ type: User })
     show(@Param('id') id: string): User {
         return this.usersService.find(Number(id));
     }
