03efed98b05f382b65347325c98915251ae7ce3d
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-24 04:17:28 +0300

    🛠️refactor(ormconfig) update synchronize option and migrations ⚙️chore(package.json) add typeorm scripts for migrations 🛠️refactor(user.entity) add email column

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,7 +3,10 @@ const config: SqliteConnectionOptions = {
     type: 'sqlite',
     database: 'db',
     entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: true
+    synchronize: false,
+    migrations: [
+        'dist/src/db/migrations/*.js'
+    ]
 }
 
 export default config;
